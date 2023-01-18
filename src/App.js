b8c45c64b6d85c753ef0aa5f84a97aa218b691a2
DESKTOP-1J61MRF\phong <phonghqm@gmail.com>
2023-01-18 20:53:51 +0700

    create header - header_HOC

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import User from "./components/UserManagement/User";
+import UserPage from "./pages/User/UserPage";
+import HeaderOnly from "./HOC/HeaderOnly/HeaderOnly";
 import Homepages from "./pages/home/Homepages";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import JobPage from "./pages/Job/JobPage";
 
 function App() {
   return (
@@ -11,7 +15,30 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />}></Route>
           <Route path="/signup" element={<Register />} />
-          <Route path="/home" element={<Homepages />}></Route>
+          <Route
+            path="/home"
+            element={
+              <HeaderOnly>
+                <Homepages />
+              </HeaderOnly>
+            }
+          ></Route>
+          <Route
+            path="/user"
+            element={
+              <HeaderOnly>
+                <UserPage />
+              </HeaderOnly>
+            }
+          ></Route>
+          <Route
+            path="/job"
+            element={
+              <HeaderOnly>
+                <JobPage />
+              </HeaderOnly>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import fiverr_logo from "../../assets/images/Font-Fiverr-Logo.jpg";
+
+export default function Header() {
+  let { name } = useSelector((state) => {
+    return state.userSlice.user.user;
+  });
+
+  return (
+    <header>
+      <div className="container flex justify-between items-center py-5 border-b-2">
+        <div
+          className="header-left flex items-center font-semibold text-lg
+        "
+        >
+          <div className="ml-3 mr-3">
+            <NavLink to="/">
+              <img className="w-28 " src={fiverr_logo} alt="" />
+            </NavLink>
+          </div>
+          <div className="ml-3 mr-3">
+            <NavLink to="/user">User</NavLink>
+          </div>
+          <div className="ml-3 mr-3">
+            <NavLink to="/job">Job</NavLink>
+          </div>
+          <div className="ml-3 mr-3">
+            <NavLink to="/jobType">Job Type</NavLink>
+          </div>
+          <div className="ml-3 mr-3">
+            <NavLink to="services">Services</NavLink>
+          </div>
+        </div>
+        <div className="header-right flex items-center">
+          <div className="mr-3">
+            <i className="fa fa-cog"></i>
+          </div>
+          <div className="bg-gray-400 user-avatar p-1 text-center">
+            {name?.slice(0, 2)}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
